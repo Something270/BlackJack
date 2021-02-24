f867871532abe21b59fbb6c5531d4830e8233acc
Something270 <55809521+Something270@users.noreply.github.com>
2021-02-24 14:14:34 -0700

    Se agrego avances de array, requiere ajustes

--- Program.cs
+++ Program.cs
@@ -12,11 +12,15 @@ namespace BlackJack
             Console.WriteLine("Press 1 to draw.");
             int option = Convert.ToInt32(Console.ReadLine());
             return option;
-        }
-        
+        }      
         static void Main(string[] args)
         {
             int option = StartOption();
+            // Int32[] values;
+            // values = new int[2];
+            // values[0] = rndoption;
+            // values[1] = option;
+            
             if (option == 1)
             {
             Random rnd = new Random();
@@ -24,7 +28,6 @@ namespace BlackJack
             Console.WriteLine("Current number: " + rndoption );
             }
 
-            
         }
     }
 }

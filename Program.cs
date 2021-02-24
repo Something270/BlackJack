76a600f8eef6382abf7b0a7dbbc172a3db416ff6
Something270 <55809521+Something270@users.noreply.github.com>
2021-02-24 16:53:46 -0700

    Added Writelines

--- Program.cs
+++ Program.cs
@@ -13,20 +13,22 @@ namespace BlackJack
             int option = Convert.ToInt32(Console.ReadLine());
             return option;
         }      
+
         static void Main(string[] args)
         {
             int option = StartOption();
-            // Int32[] values;
-            // values = new int[2];
-            // values[0] = rndoption;
-            // values[1] = option;
-            
+             Int32[] values;
+             values = new int[2];
             if (option == 1)
             {
             Random rnd = new Random();
             int rndoption = rnd.Next(1, 11);
+            values[0] = rndoption;
             Console.WriteLine("Current number: " + rndoption );
             }
+            Console.WriteLine("(1)Draw again?");
+            Console.WriteLine("(2)Hold cards?");
+            
 
         }
     }

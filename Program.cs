9a0623884938ce98e8c1f950708dec3e163bf6df
Something270 <55809521+Something270@users.noreply.github.com>
2021-02-25 11:43:58 -0700

    Added sum and array

--- Program.cs
+++ Program.cs
@@ -4,32 +4,46 @@ namespace BlackJack
 {
     class Program
     {
+        static int sum = 0;
         /// <summary>
         /// Asks to press one to draw card
         /// </summary>
         /// <returns> 1 to draw a card </returns>
-        static int StartOption(){
+        static int StartOption()
+        {
             Console.WriteLine("Press 1 to draw.");
             int option = Convert.ToInt32(Console.ReadLine());
             return option;
-        }      
-
+        }
+        /// <summary>
+        /// suma nuevos cartas con la carta presente
+        /// </summary>
+        /// <param name="rndoption">Es la carta random</param>
+        static void AddSum(int rndoption)
+        {
+            sum = sum + rndoption;
+        }
         static void Main(string[] args)
         {
             int option = StartOption();
-             Int32[] values;
-             values = new int[2];
-            if (option == 1)
+            Int32[] values;
+            values = new int[1];
+            while (option == 1)
             {
-            Random rnd = new Random();
-            int rndoption = rnd.Next(1, 11);
-            values[0] = rndoption;
-            Console.WriteLine("Current number: " + rndoption );
+                Random rnd = new Random();
+                int rndoption = rnd.Next(1, 11);
+                values[0] = rndoption;
+                AddSum(rndoption);
+                Console.WriteLine("Current number: " + sum);
+                Console.WriteLine("Press 1 to draw again");
+                Console.WriteLine("Press 2 to hold");
+                option = Convert.ToInt32(Console.ReadLine());
+                
+                if(sum >=21)
+                {
+                    break;
+                }
             }
-            Console.WriteLine("(1)Draw again?");
-            Console.WriteLine("(2)Hold cards?");
-            
-
         }
     }
 }
